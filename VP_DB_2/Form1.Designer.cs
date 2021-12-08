bb21be3ac2f498e0abbf531efd363f8040a9c9a8
Adi Maaita <adi.maaita80@gmail.com>
2021-12-08 10:28:11 +0200

    Add project files.

---
+++ VP_DB_2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace VP_DB_2
+{
+    partial class frmStudentsList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblStudentNo = new System.Windows.Forms.Label();
+            this.txtStudentNo = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.StudentId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StudentId,
+            this.StudentNo,
+            this.StudentFirstName,
+            this.StudentLastName});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(617, 427);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblStudentNo
+            // 
+            this.lblStudentNo.AutoSize = true;
+            this.lblStudentNo.Location = new System.Drawing.Point(13, 13);
+            this.lblStudentNo.Name = "lblStudentNo";
+            this.lblStudentNo.Size = new System.Drawing.Size(65, 13);
+            this.lblStudentNo.TabIndex = 1;
+            this.lblStudentNo.Text = "Student No.";
+            // 
+            // txtStudentNo
+            // 
+            this.txtStudentNo.Location = new System.Drawing.Point(84, 10);
+            this.txtStudentNo.Name = "txtStudentNo";
+            this.txtStudentNo.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentNo.TabIndex = 2;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(84, 58);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(202, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(20, 61);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(58, 13);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(367, 58);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(202, 20);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(304, 61);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(57, 13);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(84, 110);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 7;
+            this.btnAddStudent.Text = "Add Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // StudentId
+            // 
+            this.StudentId.DataPropertyName = "StudentId";
+            this.StudentId.HeaderText = "ID";
+            this.StudentId.Name = "StudentId";
+            // 
+            // StudentNo
+            // 
+            this.StudentNo.DataPropertyName = "StudentNo";
+            this.StudentNo.HeaderText = "Student Number";
+            this.StudentNo.Name = "StudentNo";
+            // 
+            // StudentFirstName
+            // 
+            this.StudentFirstName.DataPropertyName = "StudentFirstName";
+            this.StudentFirstName.HeaderText = "First Name";
+            this.StudentFirstName.Name = "StudentFirstName";
+            // 
+            // StudentLastName
+            // 
+            this.StudentLastName.DataPropertyName = "StudentLastName";
+            this.StudentLastName.HeaderText = "Last Name";
+            this.StudentLastName.Name = "StudentLastName";
+            // 
+            // frmStudentsList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 600);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtStudentNo);
+            this.Controls.Add(this.lblStudentNo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmStudentsList";
+            this.Text = "Students List";
+            this.Load += new System.EventHandler(this.frmStudentsList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblStudentNo;
+        private System.Windows.Forms.TextBox txtStudentNo;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentLastName;
+    }
+}
+
---
+++ VP_DB_2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_DB_2
+{
+    public partial class frmStudentsList : Form
+    {
+
+        private string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\user\\Documents\\StudentsDB.accdb";
+        private OleDbConnection con;
+
+        public frmStudentsList()
+        {
+            con = new OleDbConnection(connectionString);
+            InitializeComponent();
+        }
+
+        private void frmStudentsList_Load(object sender, EventArgs e)
+        {
+            UpdateStudentsGrid();
+        }
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+            InsertStudent(txtStudentNo.Text, txtFirstName.Text, txtLastName.Text);
+            UpdateStudentsGrid();
+        }
+
+        private void InsertStudent(
+            string studentNo, 
+            string studentFirstName, 
+            string studentLastName)
+        {
+            string insertCommand = $"Insert Into Students(StudentNo, StudentFirstName, StudentLastName) " +
+                $"values('{studentNo}', '{studentFirstName}', '{studentLastName}')";
+
+            OleDbCommand cmd = new OleDbCommand(insertCommand, con);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        private void UpdateStudentsGrid()
+        {
+            OleDbCommand cmd = new OleDbCommand("Select * from Students", con);
+
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
